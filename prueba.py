c77878ff37bb12bcae9d1278eb2b6d45ccab7d47
CMMProfeDuoc <ca.felipe@profesor.duoc.cl>
2023-06-14 10:40:08 -0400

    prueba

---
+++ prueba.py
@@ -0,0 +1,30 @@
+"""
+Una pizzeria se instala en DUOC. Es su trabajo crear un programa para realizar pedidos
+
+Para esto, el programa debe mostrar la lista de pizzas (dada a usted) y el precio de cada una de estas.
+Luego, debe preguntar al usuario que tipo de usuario es.
+
+Una vez que el usuario elije todas las pizas que desee comprar, se le aplicarán
+descuentos segun lo siguiente:
+
+Si el usuario es tipo "estudiante": 20% de descuento al total de la compra
+Si el usuario es tipo "profesor": 10% de descuento al total de la compra
+
+Si lleva 3 pizzas "Napolitanas", solo paga 2
+Si lleva 2 pizzas de "Pepperoni", 10% de descuento al total de la compra
+
+Los descuentos se acumulan.
+
+Una vez que un usuario compra sus pizzas, se puede pasar a otro usuario o finalizar el programa.
+El programa debe mostrar el total de las ganancias (las ventas totales de todos los usuarios)
+
+Puede asumir que el usuario ingresa bien los datos
+NO puede usar ninguna libreria
+"""
+
+#Pizas y precios de cada una
+# Tradicional 12000
+# Pepperoni 14000
+# Margarita 12500
+# Napolitana 11000
+# Carnes 17000
